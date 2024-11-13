9164079b6d4dcabbaeb1ee3f26778d6db2496abd
Josue Vindas <josue.vindas.perez@est.una.ac.cr>
2024-11-13 17:38:55 -0600

    agregado todo lo de jest y corregir elemntos de BiesC.test.js

--- test/BiesC.test.js
+++ test/BiesC.test.js
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 import fs from 'fs';
-import path from 'path';
+
 
 const testFiles = [
     'test1.bies',
@@ -8,18 +8,20 @@ const testFiles = [
     'testFunciones.bies',
     'testIF.bies',
     'testLogic.bies'
+
 ];
 
-describe('BiesVM Test Suite', () => {
+
     testFiles.forEach((file) => {
         test(`Testing ${file}`, (done) => {
-            const filePath = path.join(__dirname, 'test', file); // Adjust path if needed
+           
+            const filePath = `./test/${file}`; // Adjust the path if needed
             
-            // Check if file exists
-            expect(fs.existsSync(filePath)).toBe(true);
 
-            // Execute each test file in biesVM
-            exec(`node ./biesC.js -o ${filePath}`, (error, stdout, stderr) => {
+            expect(fs.existsSync(filePath)).toBe(true);
+           
+            exec(`node  ./commander.js  ${file}`, (error, stdout, stderr) => {
+                
                 expect(error).toBeNull(); // Expect no errors
                 expect(stderr).toBe(''); // Expect no standard error output
                 console.log(`Output for ${file}:\n${stdout}`); // Optional: Display output for each test
@@ -27,4 +29,4 @@ describe('BiesVM Test Suite', () => {
             });
         });
     });
-});
+
