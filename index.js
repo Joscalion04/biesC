e1a7a40bad4e72ea77d45cd21a00a4c529cc5de6
Joscalion04 <joscalion04@gmail.com>
2024-11-14 09:38:47 -0600

    REESTRUCTURA TESTING

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import parseBIESCode from './src/runner.mjs';
 
-parseBIESCode("./test/test1.bies");
+parseBIESCode("./test/test_unitarios/test1.bies");
 
 import fs from 'fs'; // Importar el módulo fs para trabajar con archivos
 
